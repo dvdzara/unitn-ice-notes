1aa30233ba2cf67f2f58c6e2eb4339c32f2b0caf
Davide Zarantonello <davide@zarantonello.dev>
2024-09-15 13:36:54 +0000

    feat: switch to building with startlight

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import starlight from "@astrojs/starlight";
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    starlight({
+      credits: true,
+      customCss: ["./src/styles/globals.css"],
+      editLink: {
+        baseUrl: "https://git.zarantonello.dev/davide/uni/-/edit/main",
+      },
+      favicon: "/favicon.svg",
+      lastUpdated: true,
+      pagination: false,
+      social: {
+        gitlab: "https://git.zarantonello.dev/davide",
+      },
+      title: "ICE Notes",
+      sidebar: [
+        {
+          label: "Courses",
+          autogenerate: { directory: "courses" },
+        },
+        {
+          label: "Lessons",
+          autogenerate: { directory: "lessons", collapsed: true },
+        },
+      ],
+    }),
+  ],
   markdown: {
-    remarkPlugins: ["remark-gfm", "remark-smartypants", "remark-math"],
-    rehypePlugins: ["rehype-mathjax"],
+    rehypePlugins: [rehypeMathjax],
+    remarkPlugins: [remarkMath],
   },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,8 @@
-import { defineCollection, reference, z } from "astro:content";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { defineCollection } from "astro:content";
 
-const courses = defineCollection({
-  type: "content",
-  schema: z.object({
-    name: z.string().min(1),
-    teachers: z.array(z.string().min(1)).min(1),
-    language: z.enum(["it", "en"]),
-    year: z.number().int().min(1),
-    semester: z.enum(["first", "second"]),
+export const collections = {
+  docs: defineCollection({
+    schema: docsSchema(),
   }),
-});
-
-const lessons = defineCollection({
-  type: "content",
-  schema: z.object({
-    course: reference("courses"),
-    date: z.date(),
-  }),
-});
-
-export const collections = { courses, lessons };
+};
