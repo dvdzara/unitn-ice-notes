3c854e830856ad7ef745001c049bfc4e92fc8ab6
Davide Zarantonello <davide@zarantonello.dev>
2024-10-24 16:15:25 +0000

    feat: friendship ended with deno, now bun is my best friend

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 import remarkMath from "remark-math";
-import rehypeTemml from "./src/lib/rehype-temml.ts";
+import rehypeKatex from "./src/lib/rehype-katex";
 
 export default defineConfig({
   site: "https://ice-notes.zarantonello.dev",
   markdown: {
-    rehypePlugins: [rehypeTemml],
+    rehypePlugins: [rehypeKatex],
     remarkPlugins: [remarkMath],
   },
   integrations: [
@@ -20,6 +20,16 @@ export default defineConfig({
       social: {
         github: "https://github.com/dvdzara/unitn-ice-notes",
       },
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "stylesheet",
+            href: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css",
+            crossorigin: "anonymous",
+          },
+        },
+      ],
       sidebar: [
         "Analisi 1",
         "Geometria e algebra lineare",
