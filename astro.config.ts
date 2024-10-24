7cf96959c942e2594f41d9836a910cff717a5cbf
Davide Zarantonello <davide@zarantonello.dev>
2024-10-24 13:04:07 +0000

    refactor: migrate project to deno
    The only thing missing is package.json -> deno.json

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,11 @@ import starlight from "@astrojs/starlight";
 
 import remarkMath from "remark-math";
 import rehypeTemml from "./src/lib/rehype-temml";
-import rehypeMermaid from "rehype-mermaid";
 
 export default defineConfig({
   site: "https://ice-notes.zarantonello.dev",
   markdown: {
-    rehypePlugins: [
-      rehypeTemml,
-      [rehypeMermaid, { dark: true, strategy: "img-png" }],
-    ],
+    rehypePlugins: [rehypeTemml],
     remarkPlugins: [remarkMath],
   },
   integrations: [
